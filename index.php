2551e2376cfc17cbd94cdc2c486f8d61d949d286
Sam Im <samuelim@gmail.com>
2013-09-16 02:08:35 -0700

    editing style, box and colors

--- index.php
+++ index.php
@@ -7,23 +7,24 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<div id="name">
-		<h1>Sam Im</h1>
+	<div id="container">
+		<div id="name">
+			<h1>Sam Im</h1>
+		</div>
+		<div id="about">
+			<h2>About Me</h2>
+			<p>
+				I grew up in California and currently reside in Northern California. I work at Northrop Grumman as a Quality Engineer. I enjoy learning, reading, hiking, watching movies, and experiencing the arts! I intend on completing the ALM in IT by May 2014 and have had a wonderful experience with the program.
+			</p>
+			<h2>Code Editors</h2>
+			<p>
+				The code editors I use are: Notepad++, PHPEdit, and Visual Studio.
+			</p>
+			<p>
+				<img src="img/greeneggs.jpg" alt="green eggs and ham" />
+			</p>
+		</div>
 	</div>
-	<div id="about">
-		<h2>About Me</h2>
-		<p>
-			I grew up in California and currently reside in Northern California. I work at Northrop Grumman as a Quality Engineer. I enjoy learning, reading, hiking, watching movies, and experiencing the arts! I intend on completing the ALM in IT by May 2014 and have had a wonderful experience with the program.
-		</p>
-		<h2>Code Editors</h2>
-		<p>
-			The code editors I use are: Notepad++, PHPEdit, and Visual Studio.
-		</p>
-		<p>
-			<img src="img/greeneggs.jpg" alt="green eggs and ham" />
-		</p>
-	</div>
-
 </body>
 </html>
 
